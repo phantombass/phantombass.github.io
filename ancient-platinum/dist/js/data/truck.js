4049d489a9b9858aeccb5f9a878763615e4ca69c
phantombass <tmkeaton@ngu.edu>
2025-06-01 10:16:00 -0400

    calc fix?

--- ancient-platinum/dist/js/data/truck.js
+++ ancient-platinum/dist/js/data/truck.js
@@ -1,5 +1,4 @@
 TRUCK_MESSAGES = [
-    "Are you sure you want to reset? This will clear all imported sets and change your current trainer back to Rival Silver. This cannot be undone.",
-    "Are you sure you want to reset? This will clear all imported sets and change your current trainer back to Rival Silver. This cannot be undone.",
+    "Are you sure you want to reset? This will clear all imported sets and change your current trainer back to Youngster Tristan. This cannot be undone.",
     ()=>{return `You wiped to ${window.CURRENT_TRAINER} ? Hey there, I know you just lost your run so take it easy and think about your next moves against these opponents`},
 ]
